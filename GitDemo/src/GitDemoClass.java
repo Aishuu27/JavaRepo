fb4bd22483b66941c53520d7ae4c5963e0e9e8c7
HP <aishwaryamurali11@gmail.com>
2021-03-23 18:10:45 +0530

    line 2 added

--- GitDemo/src/GitDemoClass.java
+++ GitDemo/src/GitDemoClass.java
@@ -4,6 +4,7 @@ public class GitDemoClass {
 	public void method1() {
 		System.out.println("method 1");
 		System.out.println("line1");
+		System.out.println("line 2");
 	}
 	public void method2() {
 		System.out.println("method 2");
