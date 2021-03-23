4d4450af8f446fd27b9bc913fb243b0957826cf5
HP <aishwaryamurali11@gmail.com>
2021-03-23 17:13:35 +0530

    method 3 added

--- GitDemo/src/GitDemoClass.java
+++ GitDemo/src/GitDemoClass.java
@@ -4,6 +4,12 @@ public class GitDemoClass {
 	public void method1() {
 		System.out.println("method 1");
 	}
+	public void method2() {
+		System.out.println("method 2");
+	}
+	public void  method3() {
+		System.out.println("method 3");
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
